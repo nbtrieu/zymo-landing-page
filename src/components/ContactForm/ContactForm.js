b57c40510b5aa4f810cb5383cdde8e07c3d100e9
Nicole T <nbtrieu@uci.edu>
2023-06-05 00:56:03 -0700

    Cleaned up code

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -33,77 +33,72 @@ export default function ContactForm() {
   }
   
   return (
-    <section>
-      <form onSubmit={handleFormSubmit}>
-        <div className='flex-column flex-center text-center subheading'>
-          <h1>Form</h1>
-          <div className='form-wrapper'>
-            <div className='flex-row flex-center form-rows'>
-              <div className='col-2 text-left'>
-                <h3>First Name</h3>
-                <input 
-                  type='text'
-                  name='firstName'
-                  required
-                  onChange={handleInputChange}
-                />
-              </div>
-              <div className='col-2 text-left'>
-                <h3>Last Name</h3>
-                <input 
-                  type='text'
-                  name='lastName'
-                  required
-                  onChange={handleInputChange}
-                />
-              </div>
+    <form onSubmit={handleFormSubmit}>
+      <div className='flex-column flex-center text-center subheading'>
+        <h1>Form</h1>
+        <div className='form-wrapper'>
+          <div className='flex-row flex-center form-rows'>
+            <div className='col-2 text-left'>
+              <h3>First Name</h3>
+              <input 
+                type='text'
+                name='firstName'
+                required
+                onChange={handleInputChange}
+              />
             </div>
-            <div className='flex-row flex-center mb-2 form-rows'>
-              <div className='col-2 text-left'>
-                <h3>Company</h3>
-                <input 
-                  type='text'
-                  name='company'
-                  required
-                  onChange={handleInputChange}
-                />
-              </div>
-              <div className='col-2 text-left'>
-                <h3>Role</h3>
-                <input 
-                  type='text'
-                  name='role'
-                  required
-                  onChange={handleInputChange}
-                />
-              </div>
-            </div>
-            <div className='flex-row flex-center mb-2 form-rows'>
-              <div className='col-1 text-left comment-box'>
-                <h3>Additional Comments</h3>
-                <textarea 
-                  type='text'
-                  name='comments'
-                  onChange={handleInputChange}
-                />
-              </div>
+            <div className='col-2 text-left'>
+              <h3>Last Name</h3>
+              <input 
+                type='text'
+                name='lastName'
+                required
+                onChange={handleInputChange}
+              />
             </div>
           </div>
-          <div className='submit-wrapper'>
-            <div className='flex-row flex-center mb-2'>
-              <button>
-                Submit
-              </button>
+          <div className='flex-row flex-center mb-2 form-rows'>
+            <div className='col-2 text-left'>
+              <h3>Company</h3>
+              <input 
+                type='text'
+                name='company'
+                required
+                onChange={handleInputChange}
+              />
             </div>
-            <div className='mb-3 message'>
-              <p>{message}</p>
+            <div className='col-2 text-left'>
+              <h3>Role</h3>
+              <input 
+                type='text'
+                name='role'
+                required
+                onChange={handleInputChange}
+              />
             </div>
           </div>
-          
+          <div className='flex-row flex-center mb-2 form-rows'>
+            <div className='col-1 text-left comment-box'>
+              <h3>Additional Comments</h3>
+              <textarea 
+                type='text'
+                name='comments'
+                onChange={handleInputChange}
+              />
+            </div>
+          </div>
         </div>
-      </form>
-    </section>
-    
-    
+        <div className='submit-wrapper'>
+          <div className='flex-row flex-center mb-2'>
+            <button>
+              Submit
+            </button>
+          </div>
+          <div className='mb-3 message'>
+            <p>{message}</p>
+          </div>
+        </div>
+      </div>
+    </form>
   )
 }
